9629bd2be780e7132341075a987ae99a51b09b25
Frank Gerke <fgerke@gmail.com>
2026-01-01 17:00:17 -0300

    Fix: automate superuser creation for free tier

--- build.sh
+++ build.sh
@@ -6,3 +6,14 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Cria o superusuário automaticamente se ele não existir
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'fgerke@gmail.com', 'Lulu,1502')
+    print("Superuser created successfully!")
+else:
+    print("Superuser already exists.")
+EOF
